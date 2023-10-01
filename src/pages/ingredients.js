436936bb842943a1d85d280c0819e4debeca525f
iahudson <iah224@lehigh.edu>
2023-10-01 13:22:21 -0400

    tabs displaying accurately, modify quantity

--- src/pages/ingredients.js
+++ src/pages/ingredients.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React, { Fragment, useState, useEffect } from "react";
 import logo from "../imgs/logo-no-text.png";
-import apple from "../imgs/apple2.png";
 import { Container } from "react-bootstrap";
+import { config } from "../Constants";
 import "./ingredients.css";
 import {
   usePopupState,
@@ -12,16 +12,18 @@ import {
 import Menu from "@mui/material/Menu";
 import Card from "react-bootstrap/Card";
 import { Tabs, Tab } from "react-bootstrap";
-import { config } from "../Constants";
 
 const Ingredients = () => {
-  var baseURL = config.url.API_HOME;
   const [inputs, setInputs] = useState([]); // initialize as an empty array
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [quantity, setQuantity] = useState("");
   const [activeTab, setActiveTab] = useState("first");
+  const [pantryList, setPantryList] = useState([]);
+
+  const url = process.env.MONGODB_URL;
+  var urlB = config.url.API_HOME;
 
   const handleTabChange = (tabKey) => {
     setActiveTab(tabKey);
@@ -32,7 +34,7 @@ const Ingredients = () => {
   const uid = loginData.id;
 
   const getInputs = async () => {
-    const response = await fetch(`${baseURL}/${uid}/ingredients/`);
+    const response = await fetch(`${urlB}/${uid}/ingredients/`);
     const jsonData = await response.json();
 
     setInputs(jsonData);
@@ -50,7 +52,7 @@ const Ingredients = () => {
   const onSubmitForm = async (e) => {
     e.preventDefault();
     const response = await fetch(
-      `${baseURL}/ingredients/${name}/${price}/${category}/${quantity}/${uid}`,
+      `${urlB}/ingredients/${name}/${price}/${category}/${quantity}/${uid}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -59,9 +61,55 @@ const Ingredients = () => {
   };
 
   const handleDelete = async (id) => {
-    await axios.delete(`${baseURL}/ingredients/${id}`);
+    await axios.delete(`${urlB}/ingredients/${id}`);
     setInputs(inputs.filter((ingredient) => ingredient._id !== id));
   };
+
+  const handleIncrement = async (_id, currentAmount) => {
+    ++currentAmount;
+
+    axios
+      .put(`${urlB}/ingredients/${_id}/${currentAmount}`)
+      .then((response) => {})
+      .catch((error) => {
+        console.error("Error updating ingredient:", error);
+      });
+
+    window.location.reload();
+  };
+
+  const handleDecrement = async (_id, currentAmount) => {
+    --currentAmount;
+
+    axios
+      .put(`${urlB}/ingredients/${_id}/${currentAmount}`)
+      .then((response) => {
+        console.log("Ingredient updated:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error updating ingredient:", error);
+      });
+
+    window.location.reload();
+  };
+
+  const user = localStorage.getItem("loginID");
+
+  const baseURL =
+    urlB +
+    "/" +
+    encodeURIComponent(user).replace("%22", "").replace("%22", "") +
+    "/pantry";
+
+  console.log(baseURL);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPantryList(response.data);
+    });
+  }, []);
+
+  if (!pantryList) setPantryList([]);
 
   return (
     <Fragment>
@@ -148,42 +196,53 @@ const Ingredients = () => {
           </Container>
         </Menu>
         <Tabs
+          className="custom-tabs"
           activeKey={activeTab}
           onSelect={handleTabChange}
-          className="custom-tabs"
         >
-          <Tab eventKey="first" title="All">
-            <div className="flex-container">
-              {inputs.map((input) => {
-                return (
-                  <div className="ingredient-flex">
-                    <img src={apple} className="ingredient-img" />
-                    <div className="ingredient-name">{input.name}</div>
-                    <br></br>
-                    <div>Owner: {input.owner}</div>
-                    <div>Quantity: {input.quantity}</div>
-                    <div>Category: {input.category}</div>
-                    <div className="price">Price: ${input.price}</div>
-                    <button
-                      className="delete-button"
-                      onClick={() => handleDelete(input._id)}
-                      name="btn2"
-                      value="supposed to delete"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-          </Tab>
-          <Tab eventKey="second" title="484 Birkel">
-            <div className="empty-tab"></div>
-          </Tab>
-          <Tab eventKey="third" title="Mom's House">
-            <div className="empty-tab"></div>
-          </Tab>
+          {pantryList.map((pantry) => (
+            <Tab title={pantry.name}></Tab>
+          ))}
         </Tabs>
+        <div className="flex-container">
+          {inputs.map((input) => {
+            return (
+              <div className="ingredient-flex">
+                <div className="ingredient-name">{input.name}</div>
+                <br></br>
+                <div>Owner: {input.owner}</div>
+                <div>
+                  Quantity:
+                  <span className="amount">{input.quantity}</span>
+                  <button
+                    className="arrow-button"
+                    onClick={() => handleDecrement(input._id, input.quantity)}
+                  >
+                    &#8722;
+                  </button>
+                  <button
+                    className="arrow-button"
+                    onClick={() => handleIncrement(input._id, input.quantity)}
+                  >
+                    &#43;
+                  </button>
+                </div>
+                <div>Category: {input.category}</div>
+                <div className="price">Price: ${input.price}</div>
+                <button className="delete-button" variant="contained">
+                  Share
+                </button>
+                <button
+                  className="delete-button"
+                  variant="contained"
+                  onClick={() => handleDelete(input._id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Fragment>
   );
