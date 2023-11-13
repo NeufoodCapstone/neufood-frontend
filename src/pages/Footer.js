8ec6a929139cb9d2e31a2d40678e47b85575574a
anishagademsky <ang324@lehigh.edu>
2023-11-13 12:54:09 -0500

    fix footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { CDBBox } from "cdbreact";
 import logo from "./logo-transparent.png";
+
 export default function Footer() {
   return (
     <div
       style={{
         backgroundColor: "#6DA34D",
+        position: "relative",
         bottom: "0",
         width: "100%",
+        padding: "0px", // Adjust the padding for the green footer
       }}
     >
       <CDBBox
@@ -18,7 +21,11 @@ export default function Footer() {
         style={{ width: "80%" }}
       >
         <CDBBox display="flex" alignItems="center" justifyContent="center">
-          <img alt="logo" src={logo} width="100px" />
+          <img
+            alt="logo"
+            src={logo}
+            style={{ maxWidth: "100px", height: "auto" }} // Adjust the maxWidth for the image
+          />
         </CDBBox>
         <CDBBox></CDBBox>
       </CDBBox>
