8c38d1b602605301b938a4ef2dccb47e0fa771a5
anishagademsky <78833277+anishagademsky@users.noreply.github.com>
2023-11-16 15:27:37 -0500

    Pantry flip (#36)
    * pantry cards flip
    
    * added view ingredients button
    
    * pantry cards flip with basic info on back - uneditable
    
    * changed route

--- src/pages/pantry.js
+++ src/pages/pantry.js
@@ -1,54 +1,71 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
-import { BsTrashFill } from "react-icons/bs";
-import Form from "react-bootstrap/Form";
-import "./button.css";
-import "./about.css";
-import Card from "react-bootstrap/Card";
-import neufoodLogo from "../imgs/logo-no-text.png";
-import defaultImg from "../imgs/pantry/pantry-card-img.jpeg";
-import { config } from "../Constants";
-import "reactjs-popup/dist/index.css";
-import Menu from "@mui/material/Menu";
-import {
-  usePopupState,
-  bindTrigger,
-  bindMenu,
-} from "material-ui-popup-state/hooks";
+// Pantry.js
 
-import "./pantry.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+import { BsTrashFill } from 'react-icons/bs';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import neufoodLogo from '../imgs/logo-no-text.png';
+import { config } from '../Constants';
+import 'reactjs-popup/dist/index.css';
+import Menu from '@mui/material/Menu';
+import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
+import './pantry.css';
 
 var url = config.url.API_HOME;
 
 function Pantry() {
-  const loginData = JSON.parse(localStorage.getItem("loginData"));
-  if (loginData == null) window.location.href = "/signin";
+
+  const handleCardClick = (e, pantry_id) => {
+    // Check if the click happened on the "View Ingredients" button
+    if (e.target.classList.contains('view-ingredients-btn')) {
+      // Clicked on the "View Ingredients" button, handle accordingly
+      viewIngredients(e, pantry_id);
+    } else {
+      setFlippedCards((prev) =>
+        prev.includes(pantry_id) ? prev.filter((id) => id !== pantry_id) : [...prev, pantry_id]
+      );
+      // Clicked elsewhere on the card, handle the click as needed
+      // For example, you can choose to do nothing or navigate to a different page
+      // window.location.href = '/some-other-page';
+    }
+  };
+
+  const loginData = JSON.parse(localStorage.getItem('loginData'));
+  if (loginData == null) window.location.href = '/signin';
 
   const goToPantry = (e) => {
     e.preventDefault();
-    window.location.href = "/ingredients";
+    window.location.href = '/ingredients/'; //what do we have to add here to make it link directly to the selected pantry page 
   };
 
-  const user = localStorage.getItem("loginID");
+  const viewIngredients = (e, pantry_id) => {
+    e.stopPropagation(); // Prevent the click event from reaching the parent container
+
+    // Add the logic to navigate to the ingredients page for the selected pantry
+    window.location.href = `/ingredients?pantry=${pantry_id}`;
+  };
+
+  const user = localStorage.getItem('loginID');
   const [pantryList, setPantryList] = useState([]);
   const popupstate = usePopupState({
-    variant: "popover",
-    popupId: "demoMenu",
+    variant: 'popover',
+    popupId: 'demoMenu',
   });
   const addNewMember = usePopupState({
-    variant: "popover",
-    popupId: "demoMenu",
+    variant: 'popover',
+    popupId: 'demoMenu',
   });
 
-  const [addedUser, setAddedUser] = useState("");
+  const [addedUser, setAddedUser] = useState('');
+  const [flippedCards, setFlippedCards] = useState([]);
 
   const baseURL =
     url +
-    "/" +
-    encodeURIComponent(user).replace("%22", "").replace("%22", "") +
-    "/pantry";
+    '/' +
+    encodeURIComponent(user).replace('%22', '').replace('%22', '') +
+    '/pantry';
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -63,7 +80,6 @@ function Pantry() {
     setPantryList(pantryList.filter((pantry) => pantry._id !== id));
   };
 
-  //add pantry
   const handleSubmit = (e) => {
     const formData = new FormData(e.currentTarget);
     e.preventDefault();
@@ -74,18 +90,18 @@ function Pantry() {
     }
     const urls =
       url +
-      "/pantry/" +
+      '/pantry/' +
       temp[0] +
-      "/" +
-      encodeURIComponent(user).replace("%22", "").replace("%22", "");
+      '/' +
+      encodeURIComponent(user).replace('%22', '').replace('%22', '');
 
     const myRequest = new Request(urls, {
-      method: "POST",
-      mode: "no-cors",
-      credentials: "include",
-      body: "null",
+      method: 'POST',
+      mode: 'no-cors',
+      credentials: 'include',
+      body: 'null',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
 
@@ -98,21 +114,19 @@ function Pantry() {
         console.log(e);
       });
   };
-  //add member
+
   const submitNewMember = (pantry_id) => {
     const temp = [];
 
     const myRequest = new Request(
-      `${baseURL}/pantry/${pantry_id}/${addedUser}/${loginData.getItem(
-        "loginID"
-      )}`,
+      `${baseURL}/pantry/${pantry_id}/${addedUser}/${loginData.getItem('loginID')}`,
       {
-        method: "POST",
-        mode: "no-cors",
-        credentials: "include",
-        body: "null",
+        method: 'POST',
+        mode: 'no-cors',
+        credentials: 'include',
+        body: 'null',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       }
     );
@@ -130,27 +144,27 @@ function Pantry() {
   return (
     <div
       style={{
-        "background-color": "#F5EFED",
+        backgroundColor: '#F5EFED',
       }}
     >
       <div
         style={{
-          display: "inline-block",
-          marginBottom: "150px",
+          display: 'inline-block',
+          marginBottom: '150px',
         }}
       >
-        <img class="header-img-ctn" src={neufoodLogo} />
+        <img className="header-img-ctn" src={neufoodLogo} />
         <br />
         <figcaption>Pantries</figcaption>
         <br></br>
         <div className="txt-ctn">
           <div className="txt-font-style">
-            {" "}
+            {' '}
             Create a new pantry + add your housemates!
           </div>
         </div>
         <button
-          class="button signin"
+          className="button signin"
           variant="contained"
           {...bindTrigger(popupstate)}
         >
@@ -181,13 +195,12 @@ function Pantry() {
       <br />
       <br />
 
-      {/* end to add a new member */}
-
       {pantryList.map((data) => (
-        <p>
-          {" "}
+        <p key={data._id}>
           <Container className="pantry-container">
-            <div className="pantry-card">
+            <div className={`pantry-card ${flippedCards.includes(data._id) ? 'flipped' : ''}`}
+              onClick={(e) => handleCardClick(e, data._id)}>
+              <div className="front-content">
               <Menu {...bindMenu(addNewMember)}>
                 <Container className="inner-pantry-container">
                   <Card className="inner-pantry-card">
@@ -199,31 +212,38 @@ function Pantry() {
                           className="join-form"
                           placeholder="Enter Valid Email:"
                         />
-                        <button class="button-3 join-btn" type="submit">
+                        <button className="button-3 join-btn" type="submit">
                           Add Member
                         </button>
+                        
                       </form>
                     </Card.Body>
+                    
                   </Card>
                 </Container>
               </Menu>
               <div className="rect-info-ctn">
-                <div className="pantry-name" onClick={goToPantry}>
+                <div className="pantry-name">
                   {data.name}
                 </div>
-                Owner: {data.owner} <br></br> Collaborators:{" "}
-                {data.member_list.length} <br></br>{" "}
+                
+                Owner: {data.owner} <br /> Collaborators: {data.member_list.length} <br />{' '}
                 <button
-                  class="add-new-membr-btn"
+                  className="add-new-membr-btn"
                   variant="contained"
                   {...bindTrigger(addNewMember)}
                 >
                   Add Member
                 </button>
+                <button
+                  className="view-ingredients-btn"
+                  onClick={(e) => viewIngredients(e, data._id)}
+                >
+                  View Ingredients
+              </button>
                 <div className="mb-3">
                   <button
-                    class="button-3"
-                    className="trash-btn-style"
+                    className="button-3 trash-btn-style"
                     onClick={() => handleDelete(data._id)}
                     name="btn2"
                     value="supposed to delete"
@@ -232,11 +252,22 @@ function Pantry() {
                   </button>
                 </div>
               </div>
+              <div className={`back-content ${flippedCards.includes(data._id) ? 'visible' : ''}`}>
+              <div className="bcontent">
+                <div className = "pantry-title">Name: {data.name}</div>
+                <div className = "owner">Owner: {data.owner} </div>
+                <div className = "collaborators">Collaborators: {data.member_list} </div>
+                <br />
+              </div>
             </div>
+            </div>
+            </div>
+          {/* Back content */}
           </Container>
         </p>
       ))}
     </div>
   );
 }
+
 export default Pantry;
