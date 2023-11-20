cb6cf332b80cfe142c6424196794ca86096f09b0
Asher Hamrick <ashhamrick2@gmail.com>
2023-11-20 13:01:29 -0500

    fix things

--- src/pages/ingredients.js
+++ src/pages/ingredients.js
@@ -31,10 +31,14 @@ const Ingredients = () => {
   const [newIngredientExpirationDate, setNewIngredientExpirationDate] =
     useState(new Date());
   const [hasSetExpirationDate, setHasSetExpirationDate] = useState(false);
-  const [selectedPantry, setSelectedPantry] = useState(null);
+  const [selectedPantry, setSelectedPantry] = useState(
+    queryParams.get("pantry") ? queryParams.get("pantry") : "All"
+  );
   const [loading, setLoading] = useState(false);
   const [loadingPantryChange, setLoadingPantryChange] = useState(false);
   const [selectedIngredient, setSelectedIngredient] = useState(null);
+
+  const [flippedCards, setFlippedCards] = useState([]);
 
   const handleFlip = (_id) => {
     setSelectedIngredient(_id);
@@ -55,6 +59,23 @@ const Ingredients = () => {
     ["Spreads", 90],
     ["Other", 14],
   ]);
+
+  const handleCardClick = (e, pantry_id) => {
+    // Check if the click happened on the "View Ingredients" button
+    if (e.target.classList.contains("view-ingredients-btn")) {
+      // Clicked on the "View Ingredients" button, handle accordingly
+      viewIngredients(e, pantry_id);
+    } else {
+      setFlippedCards((prev) =>
+        prev.includes(pantry_id)
+          ? prev.filter((id) => id !== pantry_id)
+          : [...prev, pantry_id]
+      );
+      // Clicked elsewhere on the card, handle the click as needed
+      // For example, you can choose to do nothing or navigate to a different page
+      // window.location.href = '/some-other-page';
+    }
+  };
 
   const url = process.env.MONGODB_URL;
   const urlB = config.url.API_HOME;
@@ -284,18 +305,18 @@ const Ingredients = () => {
                 onChange={(e) => setPrice(e.target.value)}
                 className="foodinput"
               />
-              {/* <input
-                    type="date"
-                    name="Expiration Date"
-                    value={newIngredientExpirationDate}
-                    placeholder="Price"
-                    onChange={(e) => {
-                      setHasSetExpirationDate(true);
-                      setNewIngredientExpirationDate(e.target.value);
-                    }}
-                    className="foodinput"
-                  />
-                  <p>Expiration Date</p> */}
+              <input
+                type="date"
+                name="Expiration Date"
+                value={newIngredientExpirationDate}
+                placeholder="Price"
+                onChange={(e) => {
+                  setHasSetExpirationDate(true);
+                  setNewIngredientExpirationDate(e.target.value);
+                }}
+                className="foodinput"
+              />
+              <p>Expiration Date</p>
               <label htmlFor="quantity" className="name">
                 Quantity:
               </label>
@@ -344,24 +365,27 @@ const Ingredients = () => {
         >
           <button
             className={`button all-ingredients ${
-              selectedPantry === null ? "selected" : ""
+              selectedPantry === "All" ? "selected" : ""
             }`}
             title="All Ingredients"
             onClick={() => handlePantryButtonClick("All")}
           >
             All Ingredients
           </button>
-          {pantryList.map((pantry) => (
-            <button
-              className={`button pantries ${
-                selectedPantry === pantry._id ? "selected" : ""
-              }`}
-              onClick={() => handlePantryButtonClick(pantry._id)}
-              key={pantry._id}
-            >
-              {pantry.name}
-            </button>
-          ))}
+          {pantryList.map((pantry) => {
+            console.log("things", selectedPantry, pantry._id);
+            return (
+              <button
+                className={`button pantries ${
+                  selectedPantry === pantry._id ? "selected" : ""
+                }`}
+                onClick={() => handlePantryButtonClick(pantry._id)}
+                key={pantry._id}
+              >
+                {pantry.name}
+              </button>
+            );
+          })}
         </div>
         <div className="flex-container">
           {/* Search bar */}
@@ -386,45 +410,74 @@ const Ingredients = () => {
               .filter((input) =>
                 input.name.toLowerCase().includes(searchInput.toLowerCase())
               )
-              .map((input) => (
-                <div className="ingredient-flex" key={input._id}>
-                  <div className="ingredient-buttons">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="28"
-                      height="28"
-                      viewBox="0 0 28 28"
-                      fill="none"
+              .map((input) => {
+                return (
+                  <div
+                    className={`ingredient-flex ${
+                      flippedCards.includes(input._id) ? "flipped" : ""
+                    }`}
+                    key={input._id}
+                    onClick={(e) => handleCardClick(e, input._id)}
+                  >
+                    <div className="ingredient-buttons">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28"
+                        height="28"
+                        viewBox="0 0 28 28"
+                        fill="none"
+                      >
+                        <path
+                          d="M13.1063 6.90967L13.8134 7.61678L14.5205 6.90967L15.448 5.98217C15.4482 5.98201 15.4483 5.98184 15.4485 5.98167C16.4828 4.9514 17.8833 4.37292 19.3432 4.37292C20.8026 4.37292 22.2026 4.95101 23.2369 5.98065C24.2673 7.01639 24.8458 8.418 24.8458 9.87907C24.8458 11.3404 24.2671 12.7422 23.2363 13.778C23.2361 13.7783 23.2358 13.7786 23.2355 13.7788L13.8134 23.201L4.39127 13.7788C4.391 13.7786 4.39072 13.7783 4.39045 13.778C3.35968 12.7422 2.78101 11.3404 2.78101 9.87907C2.78101 8.41805 3.35945 7.01648 4.38982 5.98075C5.42407 4.95105 6.8241 4.37292 8.28359 4.37292C9.74351 4.37292 11.1439 4.9514 12.1783 5.98167C12.1785 5.98184 12.1786 5.98201 12.1788 5.98217L13.1063 6.90967Z"
+                          stroke="black"
+                          stroke-width="2"
+                        />
+                      </svg>
+                    </div>
+                    <img
+                      src={getIngredientImage(input.category)}
+                      alt={`${input.category} Image`}
+                      className="ingredient-image"
+                    />
+                    <div className="ingredient-name">{input.name}</div>
+                    <br></br>
+                    <div className="amount">
+                      Q:
+                      <span>{input.quantity}</span>
+                      <button
+                        className="use-button"
+                        variant="contained"
+                        onClick={() =>
+                          handleDecrement(input._id, input.quantity - 1)
+                        }
+                      >
+                        Use
+                      </button>
+                    </div>
+                    <div
+                      className={`back-content ${
+                        flippedCards.includes(input._id) ? "visible" : ""
+                      }`}
                     >
-                      <path
-                        d="M13.1063 6.90967L13.8134 7.61678L14.5205 6.90967L15.448 5.98217C15.4482 5.98201 15.4483 5.98184 15.4485 5.98167C16.4828 4.9514 17.8833 4.37292 19.3432 4.37292C20.8026 4.37292 22.2026 4.95101 23.2369 5.98065C24.2673 7.01639 24.8458 8.418 24.8458 9.87907C24.8458 11.3404 24.2671 12.7422 23.2363 13.778C23.2361 13.7783 23.2358 13.7786 23.2355 13.7788L13.8134 23.201L4.39127 13.7788C4.391 13.7786 4.39072 13.7783 4.39045 13.778C3.35968 12.7422 2.78101 11.3404 2.78101 9.87907C2.78101 8.41805 3.35945 7.01648 4.38982 5.98075C5.42407 4.95105 6.8241 4.37292 8.28359 4.37292C9.74351 4.37292 11.1439 4.9514 12.1783 5.98167C12.1785 5.98184 12.1786 5.98201 12.1788 5.98217L13.1063 6.90967Z"
-                        stroke="black"
-                        stroke-width="2"
-                      />
-                    </svg>
+                      <div className="bcontent">
+                        <div className="pantry-title">Name: {input.name}</div>
+                        <div className="owner">Owner: {input.owner} </div>
+                        <div className="collaborators">
+                          Quantity: {input.quantity}{" "}
+                        </div>
+                        <div className="collaborators">
+                          Price: {input.price}{" "}
+                        </div>
+                        <div className="collaborators">
+                          Expiration Date:{" "}
+                          {input.expiration_date.substring(0, 10)}{" "}
+                        </div>
+                        <br />
+                      </div>
+                    </div>
                   </div>
-                  <img
-                    src={getIngredientImage(input.category)}
-                    alt={`${input.category} Image`}
-                    className="ingredient-image"
-                  />
-                  <div className="ingredient-name">{input.name}</div>
-                  <br></br>
-                  <div className="amount">
-                    Q:
-                    <span>{input.quantity}</span>
-                    <button
-                      className="use-button"
-                      variant="contained"
-                      onClick={() =>
-                        handleDecrement(input._id, input.quantity - 1)
-                      }
-                    >
-                      Use
-                    </button>
-                  </div>
-                </div>
-              ))
+                );
+              })
           )}
         </div>
       </div>
